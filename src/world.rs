9390226a8af9a9e7edb2c291e4e7c2e746c100c1
Gage Howe <howe.gaged@gmail.com>
2024-03-07 12:26:44 -0600

    Changed world to grid for O(1) access

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,8 @@
 // defines the world and its components
 use std::collections::HashMap;
 use crate::organism::Organism;
+use crate::block::Block;
+use crate::cell::Cell;
 
 pub enum VoxelType {
     Empty,
@@ -15,43 +17,25 @@ pub struct Voxel {
     warmth: f32,
 }
 
+pub enum Entity {
+    Block(Block),
+    Cell(Cell),
+}
+
 pub struct World {
-    cells: Vec<Vec<Vec<Voxel>>>,
-    organisms: HashMap<(usize, usize, usize), Organism>,
+    grid: Vec<Vec<Vec<Option<Entity>>>>,
     width: usize,
     height: usize,
     depth: usize,
 }
-
 impl World {
     pub fn new(width: usize, height: usize, depth: usize) -> World {
-        let mut cells = Vec::with_capacity(width);
-        for _ in 0..width {
-            let mut row = Vec::with_capacity(height);
-            for _ in 0..height {
-                let mut column = Vec::with_capacity(depth);
-                for _ in 0..depth {
-                    column.push(Voxel {
-                        voxel_type: VoxelType::Empty,
-                        warmth: 0.0,
-                    });
-                }
-                row.push(column);
-            }
-            cells.push(row);
-        }
+        let grid = vec![vec![vec![None; depth]; height]; width];
         World {
-            cells,
-            organisms: HashMap::new(),
+            grid,
             width,
             height,
             depth,
-        }
-    }
-    pub fn move_organism(&mut self, old_x: usize, old_y: usize, old_z: usize, new_x: usize, new_y: usize, new_z: usize) {
-        println!("Moving organism from ({}, {}, {}) to ({}, {}, {})", old_x, old_y, old_z, new_x, new_y, new_z);
-        if let Some(organism) = self.organisms.remove(&(old_x, old_y, old_z)) {
-            self.organisms.insert((new_x, new_y, new_z), organism);
         }
     }
 }
